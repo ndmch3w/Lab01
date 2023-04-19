ec5b9d7564d3e1ca0b24bde4fa0bf77a2c5a5c27
redminhnguyen <130122471+redminhnguyen@users.noreply.github.com>
2023-04-19 09:11:15 +0700

    Add files via upload

---
+++ SortingArray.java
@@ -0,0 +1,28 @@
+package lab_01;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class SortingArray {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n;
+		System.out.println("Input size of array: ");
+		n = sc.nextInt();
+		
+		
+		int[] arr = new int[n];
+		System.out.println("Input elements for array: ");
+		for (int i=0; i<n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(arr);
+		
+		System.out.println("Sorted array: ");
+		for (int i=0; i<n; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		
+	}
+}
