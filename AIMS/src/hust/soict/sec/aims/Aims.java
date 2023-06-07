35027ce87156504e5ec40ecfdea37b58d35e4b35
ndmch3w <daiminh.ng.cs@gmail.com>
2023-06-07 10:44:55 +0700

    Diem danh 20214965 Nguyen Dai Minh

--- AIMS/src/hust/soict/sec/aims/Aims.java
+++ AIMS/src/hust/soict/sec/aims/Aims.java
@@ -8,25 +8,19 @@ public class Aims {
         Cart anOrder = new Cart();
 
         DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95);
-        anOrder.addDigitalVideoDisc(dvd1);
+        anOrder.addMedia(dvd1);
         DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95);
-        anOrder.addDigitalVideoDisc(dvd2);
+        anOrder.addMedia(dvd2);
         DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99);
-        anOrder.addDigitalVideoDisc(dvd3);
+        anOrder.addMedia(dvd3);
         anOrder.showBill();
 
-        anOrder.removeDigitalVideoDisc(dvd2);
-        anOrder.showBill();
-
-        DigitalVideoDisc dvd4 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95);
-        DigitalVideoDisc dvd5 = new DigitalVideoDisc("Aladin", "Animation", 18.99);
-        DigitalVideoDisc[] dvdList1 = {dvd4, dvd5};
-        anOrder.addDigitalVideoDisc(dvdList1);
+        anOrder.removeMedia(dvd2);
         anOrder.showBill();
 
         System.out.println("Number of discs: " + DigitalVideoDisc.nbDigitalVideoDisc);
 
-        System.out.println(Cart.isMatch(3));
-        System.out.println(Cart.isMatch("Aladin"));
+        System.out.println(anOrder.isMatch(3));
+        System.out.println(anOrder.isMatch("Aladin"));
     }
 }
--- AIMS/src/hust/soict/sec/aims/store/Store.java
+++ AIMS/src/hust/soict/sec/aims/store/Store.java
@@ -1,14 +1,15 @@
 package hust.soict.sec.aims.store;
 
 import hust.soict.sec.aims.media.DigitalVideoDisc;
+import hust.soict.sec.aims.media.Media;
 
 import java.util.ArrayList;
 
 public class Store {
-    private static ArrayList<DigitalVideoDisc> itemsInStore = new ArrayList<>();
+    private ArrayList<Media> itemsInStore = new ArrayList<Media>();
 
-    public ArrayList<DigitalVideoDisc> getItemsInStore(){
-        return itemsInStore;
+    public String getItemsInStore(){
+        return itemsInStore.toString();
     }
     public void addDVD(DigitalVideoDisc dvd){
         itemsInStore.add(dvd);
